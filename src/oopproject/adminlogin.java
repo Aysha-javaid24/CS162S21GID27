7117f71194df267af4e626fc46866fcd05c468c8
Ayesha7654 <ayeshashabbir7654@gmail.com>
2021-05-25 06:23:09 -0700

    java project
    frontend

--- src/oopproject/adminlogin.java
+++ src/oopproject/adminlogin.java
@@ -171,6 +171,8 @@ public class adminlogin extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        
+
         // TODO add your handling code here:
         dispose();
                 java.awt.EventQueue.invokeLater(new Runnable() {
