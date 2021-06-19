c7a64faee652a811eb46b3ff8d1c45370c3c535a
Aysha <ayshajavaid95@gmail.com>
2021-06-20 00:11:44 +0500

    updated
    final term project

---
+++ src/cs162s21gid27/BusDetails.java
@@ -0,0 +1,19 @@
+package cs162s21gid27;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aysha
+ */
+public class BusDetails {
+    protected String ArrivalCity;
+    protected String DepartureCity;
+    //////////
+    
+    
+}
